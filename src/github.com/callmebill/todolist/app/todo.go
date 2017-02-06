a068fb0c5ef5493a23d78f28ba4ff207565eaf12
Xiao Gang <huangzhigang1024@qq.com>
2017-02-06 15:28:47 +0800

    fix submodule problem

--- src/github.com/callmebill/todolist/app/todo.go
+++
@@ -1,9 +0,0 @@
-package app
-
-type Todo struct {
-	Id        int    `json:"id"` //结构体便签，用于json转换时声明Key
-	Title     string `json:"title"`
-	Completed bool   `json:"completed"`
-}
-
-type Todos []Todo
